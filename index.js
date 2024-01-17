788e58804843d4369379c447731d47eab9785ba9
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-01-17 21:25:28 +0300

    Completed Palindrome Lab

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-function isPalindrome(word) {
+function isPalindrome(string1) {
   // Write your algorithm here
-}
+  // Change string items to lowercase and remove items that are not alphabets
+  string1 = string1.replace(/[^a-z]/g, '').toLowerCase();
 
-/* 
+   /* 
   Add your pseudocode here
-*/
+  */
+  // Pointers at the beginning and at the end of the string
+  let left = 0;
+  let right = string1.length - 1;
 
-/*
-  Add written explanation of your solution here
-*/
+  // Determining if palindrome 
+  while (left < right) {
+    if (string1[left] !== string1[right]) {
+      return false; // Return false if characters are not equal
+    }
+    left++;
+    right--;
+  }
+
+  return true; 
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -20,6 +32,7 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("a"));
 }
 
 module.exports = isPalindrome;
